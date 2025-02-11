94cc9b62d090db8c21ef1a243fbafe45f27dbaa7
chandanch <chandan.cec107@gmail.com>
2025-02-11 15:58:54 +0530

    feat: add ActionButton component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ActionButton from "./components/ActionButton";
 
 function App() {
   return (
     <div>
       <h1> RGM Planner App</h1>
+      <ActionButton />
     </div>
   );
 }
