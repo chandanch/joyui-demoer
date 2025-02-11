4fdfb6dfb0231c73d8c92b16e10e5d2b8031b9de
chandanch <chandan.cec107@gmail.com>
2025-02-11 15:58:15 +0530

    refactor: remove unused files and update App component structure

---
+++ src/components/ActionButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "@mui/joy/Button";
+
+function ActionButton() {
+  const buttonText = "Create Planner";
+
+  return <Button variant="solid"> {buttonText} </Button>;
+}
+
+export default ActionButton;
