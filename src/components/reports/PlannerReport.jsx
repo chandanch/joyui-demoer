4dd7f91b08eab91dc314a12fb700ea01eff775a7
chandanch <chandan.cec107@gmail.com>
2025-02-18 17:41:20 +0530

    feat: update PlannerReport layout with new table structure and responsive design

--- src/components/reports/PlannerReport.jsx
+++ src/components/reports/PlannerReport.jsx
@@ -21,10 +21,9 @@ function PlannerReport() {
 		<Grid
 			container
 			spacing={2}
-			sx={{ flexGrow: 1 }}
-			flexDirection={'column'}
+			sx={{ display: 'flex', flexDirection: 'row' }}
 		>
-			<Grid size={6}>
+			<Grid xs={6}>
 				<Table aria-label="basic table">
 					<thead>
 						<tr>
@@ -74,7 +73,56 @@ function PlannerReport() {
 					</tbody>
 				</Table>
 			</Grid>
-			<Grid size={6}></Grid>
+			<Grid xs={6}>
+				<Table aria-label="basic table">
+					<thead>
+						<tr>
+							<th style={{ width: '40%' }}>Filament</th>
+							<th>Growth Sprout</th>
+							<th>Deviation</th>
+							<th>Distribution</th>
+							<th>Linear Value</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Tactical</td>
+							<td>159</td>
+							<td>6</td>
+							<td>24</td>
+							<td>4</td>
+						</tr>
+						<tr>
+							<td>Teciet Mean</td>
+							<td>237</td>
+							<td>9</td>
+							<td>37</td>
+							<td>4.3</td>
+						</tr>
+						<tr>
+							<td>Synopsis</td>
+							<td>262</td>
+							<td>16</td>
+							<td>24</td>
+							<td>6</td>
+						</tr>
+						<tr>
+							<td>Board Members</td>
+							<td>305</td>
+							<td>3.7</td>
+							<td>67</td>
+							<td>4.3</td>
+						</tr>
+						<tr>
+							<td>Planning Grouping</td>
+							<td>356</td>
+							<td>16</td>
+							<td>49</td>
+							<td>3.9</td>
+						</tr>
+					</tbody>
+				</Table>
+			</Grid>
 		</Grid>
 	);
 }
